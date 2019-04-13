2578e399421ea96814c1c893eb7265b3156d48cb
jk <327413510@qq.com>
2019-04-13 16:55:59 +0800

    update

---
+++ private_net/fullnode2/start.sh
@@ -0,0 +1,1 @@
+java -jar FullNode.jar -c ./private_net_config.conf
---
+++ private_net/supernode/start.sh
@@ -0,0 +1,1 @@
+java -jar FullNode.jar -p a88388f4f53bbc96baed90a0e598794d2d0f4da59d778bb0cb2dc030bec881b5 --witness -c ./private_net_config.conf
